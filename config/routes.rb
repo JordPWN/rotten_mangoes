1f1ccdce08f73da205f40cf0a4a2f3736b03f781
JordPWN <jordan.ratanadawong@gmail.com>
2016-05-24 16:00:29 +0000

    Step 2 of Rotten Mangoes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
       resources :movies
     end
 
+LighthouseForum::Application.routes.draw do
+
+  get 'posts', to: 'posts#index'
+
+  # [tons of helpful comments]
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
